0106e9b2bece92e221a9174da2a0103a2a16ea01
kadunapikin <ibakoshi27@gmail.com>
2023-06-13 12:25:41 +0100

    add multyword property to user obeject

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 // property name limitation
-let user = {
-    name: "john",
-    age: 30,
-    height: "6ft"
-};
+// let user = {
+//     name: "john",
+//     age: 30,
+//     height: "6ft"
+// };
 
 // Let’s add a boolean one:
-user.isAdmin = true;
+// user.isAdmin = true;
 
-// How to delete a property from an object
-delete user.age;
+// // How to delete a property from an object
+// delete user.age;
+
+// How to us multiword property but name must be quoted
+// let user = {
+//     name: "John",
+//     age: 30,
+//     "likes birds": true
+//   };
 
 // let key = prompt("what do you want to know about the user?", "name");
-alert( user.age);
+// alert( user["likes birds"]);
+
+// Using Square brackets to set multiword property
+let user = {};
+
+// set
+user["likes birds"] = true;
+
+// get
+alert(user["likes birds"]); 
